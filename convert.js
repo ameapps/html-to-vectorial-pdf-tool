ddbbe7d025594ffec839f498985d1a02268cac6e
Stefano Amenta <git.stefano@outlook.it>
2025-09-24 23:45:21 +0200

    Aggiunta la possibilità opzionale di mantenere un numero specifico di pagine

--- convert.js
+++ convert.js
@@ -1,5 +1,10 @@
 const puppeteer = require("puppeteer");
 const path = require("path");
+const fs = require("fs");
+const { PDFDocument } = require("pdf-lib");
+
+
+const A4HEIGHT = 1684; // Altezza A4 in pixel a 96 DPI
 
 (async () => {
   // Percorso del file HTML locale (modifica qui)
@@ -18,30 +23,43 @@ const path = require("path");
 
     // Carica l'HTML
     await page.goto(fileUrl, { waitUntil: "networkidle0" });
-    const heightIt = 3205;
-    const heightEn = 3185;
+    const heightIt = 3225;
+    const heightEn = 3275;
 
     // Genera PDF vettoriale
+    const outName = `output-${langUsed}.pdf`;
     const isSinglePage = true;
     if (isSinglePage) {
       await page.pdf({
-        path: `output-${langUsed}.pdf`,
+        path: outName,
         printBackground: true,
         width: "750px", // larghezza fissa
         height: langUsed === "it" ? `${heightIt} px` : `${heightEn} px`, // altezza grande a piacere, oppure calcolata
       });
     } else {
       await page.pdf({
-        path: `output-${langUsed}.pdf`,
+        path: outName,
         format: "A4",
         printBackground: true, // mantiene i colori di sfondo e CSS
         preferCSSPageSize: true, // usa eventuali @page CSS
       });
     }
+    // Riduco il PDF a una sola pagina (opzionale)
+    await trimPdf(outName, `output-${langUsed}-trimmed.pdf`, 1);
     await browser.close();
-    console.log(`✅ PDF generato: output-${langUsed}.pdf`);
+    console.log(`✅ PDF generato: ${outName}`);
   });
-
-
-
 })();
+
+/**Mantieni solo il numero specificato di pagine invece che esportare tutto il documento*/
+async function trimPdf(inputPath, outputPath, pageCount) {
+  const existingPdf = await fs.promises.readFile(inputPath);
+  const pdfDoc = await PDFDocument.load(existingPdf);
+
+  const newPdf = await PDFDocument.create();
+  const pages = await newPdf.copyPages(pdfDoc, [...Array(pageCount).keys()]);
+  pages.forEach((p) => newPdf.addPage(p));
+
+  const pdfBytes = await newPdf.save();
+  await fs.promises.writeFile(outputPath, pdfBytes);
+}
